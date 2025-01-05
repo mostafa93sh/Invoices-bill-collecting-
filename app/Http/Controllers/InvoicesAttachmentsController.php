5e1533df04f5bc839f3485e4dd33a0011daa9ec9
unknown <mostafa.khalel.93@gmail.com>
2025-01-05 15:30:25 +0200

    add model, controller and migration of invoices details and attachments

---
+++ app/Http/Controllers/InvoicesAttachmentsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoices_attachments;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InvoicesAttachmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invoices_attachments $invoices_attachments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invoices_attachments $invoices_attachments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Invoices_attachments $invoices_attachments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invoices_attachments $invoices_attachments)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/InvoicesDetailsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoices_details;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InvoicesDetailsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invoices_details $invoices_details)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invoices_details $invoices_details)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Invoices_details $invoices_details)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invoices_details $invoices_details)
+    {
+        //
+    }
+}
